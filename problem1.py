2634a434e32060fea9bce02bf6f9e47a30d7ec66
rahul1804 <rahul.iert09@gmail.com>
2021-12-25 14:20:23 +0530

    initial commit

--- problem1.py
+++ problem1.py
@@ -1,32 +1,43 @@
 # You are given an array of n unique integers a = a[0], a[1], ... , a[n - 1] and an integer value k. Find and print the number of pairs (a[i], a
 # [j]) where i < j and a[i] + a[j] = k.
 
+'''
+input:
+6 2 1 4 5 3
+
+output:
+2
+'''
+
 import sys
 
-def get_sum_count(a, k):
-	count = 0
-	array_size = len(a)
 
-	for i in range(0, array_size):
-		for j in range(i + 1, array_size):
-			if a[i] + a[j] == k:
-				count += 1
-	return count
+def get_sum_count(a, k):
+    count = 0
+    array_size = len(a)
+
+    for i in range(0, array_size):
+        for j in range(i + 1, array_size):
+            if a[i] + a[j] == k:
+                count += 1
+    return count
+
 
 def main():
-	stdin_list = sys.stdin.readlines()
-	input_list = []
-	for item in stdin_list:
-		val = item.strip()
-		if val:
-			int_val = int(val)
-			input_list.append(int_val)
+    stdin_list = sys.stdin.readlines()
+    input_list = []
+    for item in stdin_list:
+        val = item.strip()
+        if val:
+            int_val = int(val)
+            input_list.append(int_val)
 
-	a = input_list[1:]
-	k = input_list[0]
-	output = get_sum_count(a, k)
-	print('---- output ----')
-	print(output)
+    a = input_list[1:]
+    k = input_list[0]
+    output = get_sum_count(a, k)
+    print('---- output ----')
+    print(output)
+
 
 if __name__ == "__main__":
     main()
--- problem2.py
+++ problem2.py
@@ -18,49 +18,48 @@
 import sys
 from collections import deque
 
+
 def eval_arithmetic_exp(tokens):
 
-	token=tokens.popleft()
-	if token=='+':
-		return eval_arithmetic_exp(tokens)+eval_arithmetic_exp(tokens)
-	elif token=='-':
-		return eval_arithmetic_exp(tokens)-eval_arithmetic_exp(tokens)
-	elif token=='*':
-		return eval_arithmetic_exp(tokens)*eval_arithmetic_exp(tokens)
-	elif token=='/':
-		return eval_arithmetic_exp(tokens)/eval_arithmetic_exp(tokens)
-	else:
-		return float(token)
+    token = tokens.popleft()
+    if token == '+':
+        return eval_arithmetic_exp(tokens)+eval_arithmetic_exp(tokens)
+    elif token == '-':
+        return eval_arithmetic_exp(tokens)-eval_arithmetic_exp(tokens)
+    elif token == '*':
+        return eval_arithmetic_exp(tokens)*eval_arithmetic_exp(tokens)
+    elif token == '/':
+        return eval_arithmetic_exp(tokens)/eval_arithmetic_exp(tokens)
+    else:
+        return float(token)
 
 
 def main():
-	stdin_list = sys.stdin.readlines()
-	input_list = []
-	for item in stdin_list:
-		val = item.strip()
-		if val:
-			input_list.append(val)
+    stdin_list = sys.stdin.readlines()
+    input_list = []
+    for item in stdin_list:
+        val = item.strip()
+        if val:
+            input_list.append(val)
 
-	output_list = []
+    output_list = []
 
-	for exp in input_list:
-		try:
-			tokens = deque(exp.split())
-			exp_output = eval_arithmetic_exp(tokens)
-			if len(tokens) > 0:
-				raise Exception('Invalid Expression')
-			formated_out = format(exp_output, '.2f')
-			output_list.append(formated_out)
-		except:
-			output_list.append('error')
-			continue
+    for exp in input_list:
+        try:
+            tokens = deque(exp.split())
+            exp_output = eval_arithmetic_exp(tokens)
+            if len(tokens) > 0:
+                raise Exception('Invalid Expression')
+            formated_out = format(exp_output, '.2f')
+            output_list.append(formated_out)
+        except:
+            output_list.append('error')
+            continue
 
-	print('---- output ----')
-	for item in output_list:
-		print(item)
-
-
-if __name__=='__main__':
-	main()
+    print('---- output ----')
+    for item in output_list:
+        print(item)
 
 
+if __name__ == '__main__':
+    main()
